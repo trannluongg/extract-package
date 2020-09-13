c06e8db7378c0b8f40dd3cdc5b3b64b4cd113b28
Lương Trần Văn <luongtv@vatgia.com>
2020-09-13 11:03:05 +0700

    update extact dompdf version

--- src/Http/Controllers/ExtractController.php
+++ src/Http/Controllers/ExtractController.php
@@ -31,11 +31,8 @@ class ExtractController extends Controller
         $file = public_path('upload_cv/2020/08/28/' . $name_file . '.pdf');
 
         $options_check = config('extract.options_extract');
-
         $pdf           = new PdfToHtml($file, $options_check);
-
         $output_dir    = config('extract.options_extract.outputDir');
-
         $checkPdf      = $pdf->checkPdf($output_dir, $name_file);
 
         if ($checkPdf)
